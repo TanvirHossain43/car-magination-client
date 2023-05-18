3e5ce2eea4f4b00869d0de856a200e3780d04e87
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-05-18 23:42:15 +0600

    login page added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import login from '../../assets/Login.png'
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className='w-2/4'>
+                    <img src={login} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 className='w-2/4'">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" name='email' />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" name='password' />
+                
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
